f28b960bde165350b1d596f5caed62953fa91446
Aaron Lockhart <aaronlockhartdev@gmail.com>
2022-07-28 16:50:16 -0400

    Fixed bash syntax

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ echo "🤚  This script will setup .dotfiles for you."
 
 case "$OSTYPE" in
   darwin*) # MacOS 
+    echo "🍎  Running on MacOS"
 
     # Install Homebrew
     command -v brew >/dev/null 2>&1 || \
@@ -14,29 +15,30 @@ case "$OSTYPE" in
       which $1 &> /dev/null
 
       if [ $? -ne 0 ]; then
-        echo "➡️   Installing ${1}..."
+        echo "➡️  Installing ${1}..."
         brew install $1
 	echo "✅  Done!"
       else
-        echo "⚠️   Already installed ${1}"
+        echo "⚠️  Already installed ${1}"
       fi
     }
   ;; 
   linux*) # Linux
+    echo "🐧  Running on Linux"
 
     # Update system
-    echo "⬆️   Upgrading system..." && sudo apt update && sudo apt full-upgrade -y
+    echo "⬆️  Upgrading system..." && sudo apt update && sudo apt full-upgrade -y
 
     # Create install function for apt
     function install {
       which $1 &> /dev/null
     
       if [ $? -ne 0 ]; then
-        echo "➡️   Installing ${1}..."
+        echo "➡️  Installing ${1}..."
         sudo apt install -y $1
 	echo "✅  Done!"
       else
-       echo "⚠️   Already installed ${1}"
+       echo "⚠️  Already installed ${1}"
       fi
 }    
   ;;
@@ -52,17 +54,19 @@ install wget
 
 # Install chezmoi
 command -v chezmoi >/dev/null 2>&1 || \
-  (echo "➡️   Installing chezmoi..."
+  (echo "➡️  Installing chezmoi..." && sh -c "$(curl -fsLS https://chezmoi.io/get)")
 
 # Install oh-my-zsh
-echo "➡️   Installing oh-my-zsh..." && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "➡️  Installing oh-my-zsh..." && sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 # Install pyenv
-echo "➡️   Installing pyenv..." && (curl https://pyenv.run | bash)
+command -v pyenv >/dev/null 2>&1 || \
+  (echo "➡️  Installing pyenv..." && (curl https://pyenv.run | bash))
 
 # Install dotfiles with chezmoi
 if [ -d "$HOME/.local/share/chezmoi/.git" ]; then
-  echo "⚠️   chezmoi already initialized, reinitialize with 'chezmoi init https://github.com/aaronlockhartdev/dotfiles.git"
+  echo "⚠️  chezmoi already initialized, reinitialize with 'chezmoi init https://github.com/aaronlockhartdev/dotfiles.git"
 else
   echo "🚀  Initializing dotfiles..." && chezmoi init https://github.com/aaronlockhartdev/dotfiles.git
 fi
+
