257c1e9b9e9f9ff75beba099705b4f5f8805909e
Aaron Lockhart <aaronlockhartdev@gmail.com>
2024-02-26 17:03:17 -0500

    Began using rbw

---
+++ run_once_before_install-bitwarden.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Rust and rbw
+if [ ! command -v cargo ]; then
+    curl https://sh.rustup.rs -sSf | sh
+    source "$HOME/.cargo/env"
+fi
+cargo install rbw
+
+rbw config set base_url $BITWARDEN_HOST
+rbw config set email $BITWARDEN_EMAIL
